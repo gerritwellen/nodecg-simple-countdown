1656449c2a3d48cb878dd5fdd360004a86518041
Gerrit Wellen <gerrit.wellen@gmail.com>
2020-12-04 20:51:22 +0100

    Fixed some styling and logic issues.

--- graphics/countdown.js
+++ graphics/countdown.js
@@ -38,7 +38,7 @@ var x = setInterval(function () {
   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
   var sign;
-  if (seconds > 0) {
+  if (seconds >= 0) {
     sign = "-";
   } else {
     sign = "+";
